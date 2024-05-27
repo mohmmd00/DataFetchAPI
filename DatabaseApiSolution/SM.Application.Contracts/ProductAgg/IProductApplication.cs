52134a34ceae70f5a5ddf9d76f72dc373209ee62
mohammadhosein joshghani <130822413+WhoFelln@users.noreply.github.com>
2024-05-27 23:01:46 +0330

    created product application interface

---
+++ DatabaseApiSolution/SM.Application.Contracts/ProductAgg/IProductApplication.cs
@@ -0,0 +1,11 @@
+﻿using SM.Domain.ProductAgg;
+
+namespace SM.Application.Contracts.ProductAgg
+{
+    public interface IProductApplication
+    {
+        Product FetchChosenProductBy(int id);
+        List<Product> FetchAllProducts();
+
+    }
+}
