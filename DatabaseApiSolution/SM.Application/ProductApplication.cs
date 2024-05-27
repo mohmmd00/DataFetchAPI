ce9c6ed66d707ea345b56c6fc239e2a075890685
mohammadhosein joshghani <130822413+WhoFelln@users.noreply.github.com>
2024-05-27 23:16:45 +0330

    setup sm.application class project

---
+++ DatabaseApiSolution/SM.Application/ProductApplication.cs
@@ -0,0 +1,32 @@
+﻿using SM.Application.Contracts.ProductAgg;
+using SM.Domain.ProductAgg;
+
+namespace SM.Application
+{
+    public class ProductApplication : IProductApplication
+    {
+        private readonly IProductRepository _repository;
+
+        public ProductApplication(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Product FetchChosenProductBy(int id)
+        {
+
+            var SelectedProduct = _repository.GetProductBy(id);
+            if (SelectedProduct != null)
+            {
+                return SelectedProduct;
+            }
+
+            return null;
+        }
+
+        public List<Product> FetchAllProducts()
+        {
+            return _repository.GetAllProducts();
+        }
+    }
+}
---
+++ DatabaseApiSolution/SM.Application/ProductCategoryApplication.cs
@@ -0,0 +1,20 @@
+﻿using SM.Application.Contracts.ProductCategoryAgg;
+using SM.Domain.ProductCategoryAgg;
+
+namespace SM.Application
+{
+    public class ProductCategoryApplication : IProductCategoryApplication
+    {
+        private readonly IProductCategoryRepository _repository;
+
+        public ProductCategoryApplication(IProductCategoryRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public List<ProductCategory> FetchAllProductCategories()
+        {
+            return _repository.GetAllProductCategories();
+        }
+    }
+}
