d47eb40c86b5fc5b16c5a6cad60481638da7982d
mohammadhosein joshghani <130822413+WhoFelln@users.noreply.github.com>
2024-05-28 17:34:09 +0330

    removed unneseccery pakages and changed product api controller

--- DatabaseApiSolution/ServiceHost/Controllers/ProductsController.cs
+++ DatabaseApiSolution/ServiceHost/Controllers/ProductsController.cs
@@ -32,23 +32,5 @@ namespace ServiceHost.Controllers
 
             return ChosenProduct;
         }
-
-        // POST api/<ProductsController>
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT api/<ProductsController>/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE api/<ProductsController>/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
