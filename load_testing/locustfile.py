c44de867a29c7b15fb929ab6d86624535623208b
mohammadhosein joshghani <130822413+WhoFelln@users.noreply.github.com>
2024-06-04 00:58:05 +0330

    changed Load test

--- load_testing/locustfile.py
+++ load_testing/locustfile.py
@@ -1,6 +1,22 @@
-from locust import HttpUser, task
+import os
+from locust import HttpUser, task, between
 
-class HelloWorldUser(HttpUser):
+class WebsiteUser(HttpUser):
+    wait_time = between(1, 5)
+    product_id = os.getenv("PRODUCT_ID", 1)  
+
     @task
-    def hello_world(self):
+    def get_all_products(self):
         self.client.get("/api/Products")
+
+    @task
+    def get_selected_product(self):
+        self.client.get(f"/api/Products/{self.product_id}")
+
+    @task
+    def get_recommendation_list(self):
+        self.client.get(f"/api/Products/{self.product_id}/recommendation")
+
+if __name__ == "__main__":
+    import os
+    os.system("locust -f locustfile.py")
